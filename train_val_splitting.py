4d85b54f9f31dcd6f2b0259729df263d78468c6e
NhanBuiDuc <nhanbuiduc.work@gmail.com>
2024-02-09 02:14:14 +0700

    update

--- train_val_splitting.py
+++ train_val_splitting.py
@@ -86,10 +86,11 @@ for random_seed in random_seed_list:
         val_nrg_selected_indices = random.sample(
             val_nrg_index.tolist(), val_rg_count)
         # Get the corresponding input and label data using the selected indices
-        train_selected_nrg_data = train_dataframe.iloc[train_nrg_selected_indices, :]
-        val_selected_nrg_data = val_dataframe.iloc[val_nrg_selected_indices, :]
-        train_rg_input_data = train_dataframe.iloc[train_rg_index, :]
-        val_rg_input_data = val_dataframe.iloc[val_rg_index, :]
+        train_selected_nrg_data = image_path_and_label_dataframe.iloc[
+            train_nrg_selected_indices, :]
+        val_selected_nrg_data = image_path_and_label_dataframe.iloc[val_nrg_selected_indices, :]
+        train_rg_input_data = image_path_and_label_dataframe.iloc[train_rg_index, :]
+        val_rg_input_data = image_path_and_label_dataframe.iloc[val_rg_index, :]
         train_combined_nrg_input_data = pd.concat(
             [train_selected_nrg_data, train_rg_input_data], ignore_index=True)
         val_combined_nrg_input_data = pd.concat(
