775181422dc254f56c69305881cdd1f63f0c0c56
NhanBuiDuc <nhanbuiduc.work@gmail.com>
2024-02-09 21:23:24 +0700

    update path

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -20,7 +20,7 @@ from torchvision.models import resnet50
 class trainer_base():
     def __init__(
         self,
-        data_dir: str = "data",
+        data_dir: str = "./data/",
         num_epoch: int = 50,
         train_val_test_split: Tuple[int, int, int] = (0.8, 0.2),
         image_size: int = 256,
@@ -81,7 +81,7 @@ class trainer_base():
         self.train_gt_path = os.path.join(
             self.data_dir, "AIROGS_2024", "JustRAIGS_Train_labels.csv")
 
-        self.train_gt_path = self.train_gt_path.replace("/", "\\")
+        self.train_gt_path = self.train_gt_path.replace("'", """)
 
         self.prepare_data(kfold_index, kfold_seed)
         self.loss_fn = torch.nn.BCELoss()
