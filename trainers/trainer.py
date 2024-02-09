402126c5ac7c266c6c81839cf0657607167ae43d
NhanBuiDuc <nhanbuiduc.work@gmail.com>
2024-02-09 21:27:30 +0700

    update

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -305,14 +305,14 @@ class trainer_base():
             kfold_dir = os.path.join(
                 self.data_dir, "AIROGS_2024/fold_split_images/", f"fold_{kfold_index}")
 
-        kfold_dir = kfold_dir.replace("'", '"')
+        kfold_dir = kfold_dir.replace("/", "\\")
         train_format_csv_path = os.path.join(kfold_dir,
                                              f"train_seed_{kfold_seed}_kfold_{kfold_index}.csv")
 
-        train_format_csv_path = train_format_csv_path.replace("'", '"')
+        train_format_csv_path = train_format_csv_path.replace("/", "\\")
         val_format_csv_path = os.path.join(kfold_dir,
                                            f"val_seed_{kfold_seed}_kfold_{kfold_index}.csv")
-        val_format_csv_path = val_format_csv_path.replace("'", '"')
+        val_format_csv_path = val_format_csv_path.replace("/", "\\")
 
         self.train_df = pd.read_csv(train_format_csv_path, delimiter=",")
         self.train_image_path = self.train_df["Eye ID"]
