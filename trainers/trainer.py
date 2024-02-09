2813c97e52bd35efc822a1d57dd975c6c5a2d0ad
NhanBuiDuc <nhanbuiduc.work@gmail.com>
2024-02-10 03:46:26 +0700

    change sigmoid to relu

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -111,7 +111,7 @@ class trainer_base():
         # self.model = resnet50(weights=None, progress=True,
         #                       num_classes=1)
 
-        m = nn.Sigmoid()
+        m = nn.ReLU()
 
         for epoch in range(self.num_epoch):
             self.model.train()  # Set the self.model to training mode
