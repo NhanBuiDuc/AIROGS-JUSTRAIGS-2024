5e89de2f23bbc392dffb212bd53b619b96a71bfb
NhanBuiDuc <nhanbuiduc.work@gmail.com>
2024-02-13 09:57:25 +0700

    update file path

--- datasets/AIROGS_dataset.py
+++ datasets/AIROGS_dataset.py
@@ -72,11 +72,29 @@ class Airogs_Dataset(Dataset):
                         image_path = image_path.replace("\\", "/")
                         image = Image.open(image_path).convert(
                             'RGB')  # Adjust as needed
-
                     except FileNotFoundError:
-                        # Handle the case where both .jpg and .png files are not found
-                        print(f"Error: File not found for index {index}")
-                        # You might want to return a placeholder image or raise an exception as needed
+                        try:
+                            # If the file with .jpg extension is not found, try to open the image with .png extension
+                            image_path = os.path.join(
+                                self.train_image_path, image_name + ".JPG")
+                            # Replacing backslashes with forward slashes
+                            image_path = image_path.replace("\\", "/")
+                            image = Image.open(image_path).convert(
+                                'RGB')  # Adjust as needed
+                        except FileNotFoundError:
+                            try:
+                                # If the file with .jpg extension is not found, try to open the image with .png extension
+                                image_path = os.path.join(
+                                    self.train_image_path, image_name + ".JPEG")
+                                # Replacing backslashes with forward slashes
+                                image_path = image_path.replace("\\", "/")
+                                image = Image.open(image_path).convert(
+                                    'RGB')  # Adjust as needed
+                            except FileNotFoundError:
+                                # Handle the case where both .jpg and .png files are not found
+                                print(
+                                    f"Error: File not found for index {index}")
+                                # You might want to return a placeholder image or raise an exception as needed
 
         # Apply transformations if specified
         if image is not None:
